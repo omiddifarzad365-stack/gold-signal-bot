17082fa81e41ea5bae94518fb0a2bd161f842c90
omiddifarzad365-stack <omiddifarzad365@gmail.com>
2026-07-06 15:04:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from config import *
+import requests
+from telegram import Bot
+from config import BOT_TOKEN, CHAT_ID, API_KEY
 
-print("Gold Signal Bot Started")
+print("Starting Gold Signal Bot...")
 
-print("Symbol:", SYMBOL)
-print("Interval:", INTERVAL)
+# تست اتصال به Twelve Data
+url = f"https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=5min&outputsize=1&apikey={API_KEY}"
+
+response = requests.get(url)
+
+print("Twelve Data Status:", response.status_code)
+print(response.text)
+
+# تست ارسال پیام تلگرام
+bot = Bot(BOT_TOKEN)
+
+bot.send_message(
+    chat_id=CHAT_ID,
+    text="✅ Gold Signal Bot is Online"
+)
+
+print("Done")
