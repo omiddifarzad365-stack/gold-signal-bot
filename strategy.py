1a0197e5e917c8a89fc9a61169e664b9f041c186
omiddifarzad365-stack <omiddifarzad365@gmail.com>
2026-07-06 13:21:11 +0330

    Create strategy.py

---
+++ strategy.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pandas_ta as ta
+
+def calculate_indicators(df):
+
+    df["EMA20"] = ta.ema(df["close"], length=20)
+
+    df["EMA50"] = ta.ema(df["close"], length=50)
+
+    df["EMA200"] = ta.ema(df["close"], length=200)
+
+    adx = ta.adx(
+        high=df["high"],
+        low=df["low"],
+        close=df["close"],
+        length=14
+    )
+
+    df["ADX"] = adx["ADX_14"]
+
+    return df
